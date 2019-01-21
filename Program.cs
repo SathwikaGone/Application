6dc802ca4cd5f57a446ca8424449a141d01307a3
Sathwika <gonesathwika@gmail.com>
2019-01-21 16:06:10 -0600

    commit 1

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace application
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+              while(name==""){
+              Console.WriteLine("Enter your Name");
+              var name=Console.ReadLine();
+            }
+            Console.WriteLine("Enter your marks:");
+            Console.WriteLine("Maths:");
+            var maths=Console.ReadLine();
+            Console.WriteLine("physics:");
+            var physics=Console.ReadLine();
+            Console.WriteLine("Chemistry:");
+            var chemistry=Console.ReadLine();
+            var avg;
+            var p=false;
+            var schlolarship=0.0m;
+            //Console.WriteLine("Hello World!");
+          
+        }
+    }
+}
