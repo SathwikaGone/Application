8d0e0a6b13003099c819e63e296f31da7ee48f54
Sathwika <gonesathwika@gmail.com>
2019-01-24 15:14:26 -0600

    commit

--- Program.cs
+++ Program.cs
@@ -5,7 +5,7 @@ namespace application
    
     class Program
     {
-      public decimal fee(double p){
+      public static decimal fee(double p){
                             decimal scholarship=Convert.ToDecimal(5000-((5000*p)/100));
                             return scholarship;
                 }
@@ -43,61 +43,66 @@ namespace application
 
              avg=((maths+physics+chemistry)/300)*100;
             Console.WriteLine($"\nYour persentage is {avg}.\n");
-
-            if(avg >=70.0 && avg <= 100){
-                Console.WriteLine("Congratulations, you are eligible for scholarship \n");
-
-                 if(avg >=70.0 && avg <79.9){
+            if(avg >=90 && avg <=100){
                 c='a';
                 }
-                if(avg >=80.0 && avg <89.9){
+                    if(avg >=80.0 && avg <89.9){
                 c='b';
                 }
-                if(avg >=90 && avg <=100){
+                 if(avg >=70.0 && avg <79.9){
                 c='c';
-                if(avg >100 && avg <0){
+                }
+                  if(avg>=0 && avg <70){
                     c='d';
                 }
+                if(avg >100 && avg <0){
+                    c='e';
                 }
-               Program Z= new Program();
+               
                 switch(c){
-                    case 'c':
-                        Console.WriteLine("You will get the scholarship of $1000.\n");
-                        p=20;
-                        scholarship = Z.fee(p);
-                        Console.WriteLine($"{name},The total amount to be paid: ${scholarship}.\n");
-                           
-                        
-                        break;
-                
-                    case 'b':
-                        Console.WriteLine("You will get the scholarship of $1500.");
-                        p=30;
-                        Console.WriteLine($"{name},The total amount to be paid: ${Z.fee(p)}.\n");
-                          
-                            
-                        break;
-                
                     case 'a':
+                        Console.WriteLine("Congratulations, you are eligible for scholarship \n");
                         Console.WriteLine("You will get the scholarship of $2000.");
                         p=50;
-                        Console.WriteLine($"{name},The total amount to be paid: ${Z.fee(p)}.\n");
+                        scholarship = Program.fee(p);
+                        Console.WriteLine($"{name},The total amount to be paid: ${scholarship}.\n");
                         break;
                     
-                        }        
-            }
-            else if(avg < 70){
-                Console.WriteLine("Sorry, you are not eligible for scholarship \n");
-            }
-            else{
-               Console.WriteLine("Enter CORRECT MARKS");  
-            }
+                
+                    case 'b':
+                        Console.WriteLine("Congratulations, you are eligible for scholarship \n");
+                        Console.WriteLine("You will get the scholarship of $1500.");
+                        p=30;
+                        scholarship = Program.fee(p);
+                        Console.WriteLine($"{name},The total amount to be paid: ${scholarship}.\n");
+                        break;
+                
+                    case 'c':
+                        Console.WriteLine("Congratulations, you are eligible for scholarship \n");
+                        Console.WriteLine("You will get the scholarship of $1000.\n");
+                        p=20;
+                       scholarship = Program.fee(p);
+                        Console.WriteLine($"{name},The total amount to be paid: ${scholarship}.\n");
+                        break;
+
+                     case 'd':
+                         p=0;
+                         scholarship = Program.fee(p);
+                        Console.WriteLine($"Sorry, you are not eligible for scholarship \n{name}, You need to pay ${scholarship}.\n");
+                        break;
+            
+                    case 'e':
+                        Console.WriteLine("Enter CORRECT MARKS"); 
+                        break; 
+            
+           }
              Console.WriteLine(" Do you want to continue \n 1. If yes, press y\n 2. press any key to exit:\n ");
              var q=Console.ReadLine(); 
              var l =Convert.ToChar(q);
          if(l=='y'){
              QUIT=false;
          }else{
+             Console.WriteLine("Have a nice day :)");
              QUIT=true;
          }
             }while(QUIT==false);
